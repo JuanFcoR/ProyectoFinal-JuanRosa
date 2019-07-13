e4596950038522066cf3667c895bc9754d187ee8
Juan Francisco Rosa Cuevas <juan_rosa1@ucne.edu.do>
2019-07-13 15:50:00 -0400

    Creando las ventanas consulta y registros de usuarios con su entidad y BLL

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace ProyectoFinalAlpha
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Login());
         }
     }
 }
