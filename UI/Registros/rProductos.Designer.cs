bbfeb44d52a0d4e571047dfa7f041809278798d1
Juan Francisco Rosa Cuevas <juan_rosa1@ucne.edu.do>
2019-07-19 22:46:29 -0400

    Creando el registro Productos

---
+++ UI/Registros/rProductos.Designer.cs
@@ -0,0 +1,306 @@
+﻿namespace ProyectoFinalAlpha.UI.Registros
+{
+    partial class rProductos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.NoProductoNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.DescripcionTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.CantidadNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.MinimoNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.UnidadNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.PrecioNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.CostoNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.GananciaNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.NuevoButton = new System.Windows.Forms.Button();
+            this.GuardarButton = new System.Windows.Forms.Button();
+            this.EliminarButton = new System.Windows.Forms.Button();
+            this.BuscarButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.NoProductoNumericUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CantidadNumericUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MinimoNumericUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.UnidadNumericUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PrecioNumericUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CostoNumericUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GananciaNumericUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "NoProducto";
+            // 
+            // NoProductoNumericUpDown
+            // 
+            this.NoProductoNumericUpDown.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.NoProductoNumericUpDown.Location = new System.Drawing.Point(115, 19);
+            this.NoProductoNumericUpDown.Name = "NoProductoNumericUpDown";
+            this.NoProductoNumericUpDown.Size = new System.Drawing.Size(61, 23);
+            this.NoProductoNumericUpDown.TabIndex = 1;
+            // 
+            // DescripcionTextBox
+            // 
+            this.DescripcionTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.DescripcionTextBox.Location = new System.Drawing.Point(115, 64);
+            this.DescripcionTextBox.Name = "DescripcionTextBox";
+            this.DescripcionTextBox.Size = new System.Drawing.Size(190, 23);
+            this.DescripcionTextBox.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.label2.Location = new System.Drawing.Point(12, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Descripcion";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.label3.Location = new System.Drawing.Point(12, 109);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Cantidad";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.label4.Location = new System.Drawing.Point(12, 148);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 17);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Minimo";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.label5.Location = new System.Drawing.Point(12, 186);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 17);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Unidad";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.label6.Location = new System.Drawing.Point(12, 222);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(48, 17);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Precio";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.label7.Location = new System.Drawing.Point(12, 259);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(44, 17);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Costo";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.label8.Location = new System.Drawing.Point(12, 291);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(69, 17);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "Ganancia";
+            // 
+            // CantidadNumericUpDown
+            // 
+            this.CantidadNumericUpDown.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.CantidadNumericUpDown.Location = new System.Drawing.Point(115, 103);
+            this.CantidadNumericUpDown.Name = "CantidadNumericUpDown";
+            this.CantidadNumericUpDown.Size = new System.Drawing.Size(190, 23);
+            this.CantidadNumericUpDown.TabIndex = 10;
+            // 
+            // MinimoNumericUpDown
+            // 
+            this.MinimoNumericUpDown.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.MinimoNumericUpDown.Location = new System.Drawing.Point(115, 142);
+            this.MinimoNumericUpDown.Name = "MinimoNumericUpDown";
+            this.MinimoNumericUpDown.Size = new System.Drawing.Size(190, 23);
+            this.MinimoNumericUpDown.TabIndex = 11;
+            // 
+            // UnidadNumericUpDown
+            // 
+            this.UnidadNumericUpDown.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.UnidadNumericUpDown.Location = new System.Drawing.Point(115, 184);
+            this.UnidadNumericUpDown.Name = "UnidadNumericUpDown";
+            this.UnidadNumericUpDown.Size = new System.Drawing.Size(190, 23);
+            this.UnidadNumericUpDown.TabIndex = 12;
+            // 
+            // PrecioNumericUpDown
+            // 
+            this.PrecioNumericUpDown.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.PrecioNumericUpDown.Location = new System.Drawing.Point(115, 220);
+            this.PrecioNumericUpDown.Name = "PrecioNumericUpDown";
+            this.PrecioNumericUpDown.Size = new System.Drawing.Size(190, 23);
+            this.PrecioNumericUpDown.TabIndex = 13;
+            // 
+            // CostoNumericUpDown
+            // 
+            this.CostoNumericUpDown.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.CostoNumericUpDown.Location = new System.Drawing.Point(115, 257);
+            this.CostoNumericUpDown.Name = "CostoNumericUpDown";
+            this.CostoNumericUpDown.Size = new System.Drawing.Size(190, 23);
+            this.CostoNumericUpDown.TabIndex = 14;
+            // 
+            // GananciaNumericUpDown
+            // 
+            this.GananciaNumericUpDown.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.GananciaNumericUpDown.Location = new System.Drawing.Point(115, 289);
+            this.GananciaNumericUpDown.Name = "GananciaNumericUpDown";
+            this.GananciaNumericUpDown.Size = new System.Drawing.Size(190, 23);
+            this.GananciaNumericUpDown.TabIndex = 15;
+            // 
+            // NuevoButton
+            // 
+            this.NuevoButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.NuevoButton.Location = new System.Drawing.Point(6, 351);
+            this.NuevoButton.Name = "NuevoButton";
+            this.NuevoButton.Size = new System.Drawing.Size(75, 54);
+            this.NuevoButton.TabIndex = 16;
+            this.NuevoButton.Text = "Nuevo";
+            this.NuevoButton.UseVisualStyleBackColor = true;
+            // 
+            // GuardarButton
+            // 
+            this.GuardarButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.GuardarButton.Location = new System.Drawing.Point(115, 351);
+            this.GuardarButton.Name = "GuardarButton";
+            this.GuardarButton.Size = new System.Drawing.Size(75, 54);
+            this.GuardarButton.TabIndex = 17;
+            this.GuardarButton.Text = "Guardar";
+            this.GuardarButton.UseVisualStyleBackColor = true;
+            // 
+            // EliminarButton
+            // 
+            this.EliminarButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.EliminarButton.Location = new System.Drawing.Point(221, 351);
+            this.EliminarButton.Name = "EliminarButton";
+            this.EliminarButton.Size = new System.Drawing.Size(75, 54);
+            this.EliminarButton.TabIndex = 18;
+            this.EliminarButton.Text = "Eliminar";
+            this.EliminarButton.UseVisualStyleBackColor = true;
+            // 
+            // BuscarButton
+            // 
+            this.BuscarButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.BuscarButton.Location = new System.Drawing.Point(195, 19);
+            this.BuscarButton.Name = "BuscarButton";
+            this.BuscarButton.Size = new System.Drawing.Size(75, 23);
+            this.BuscarButton.TabIndex = 19;
+            this.BuscarButton.Text = "Buscar";
+            this.BuscarButton.UseVisualStyleBackColor = true;
+            // 
+            // rProductos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(317, 450);
+            this.Controls.Add(this.BuscarButton);
+            this.Controls.Add(this.EliminarButton);
+            this.Controls.Add(this.GuardarButton);
+            this.Controls.Add(this.NuevoButton);
+            this.Controls.Add(this.GananciaNumericUpDown);
+            this.Controls.Add(this.CostoNumericUpDown);
+            this.Controls.Add(this.PrecioNumericUpDown);
+            this.Controls.Add(this.UnidadNumericUpDown);
+            this.Controls.Add(this.MinimoNumericUpDown);
+            this.Controls.Add(this.CantidadNumericUpDown);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.DescripcionTextBox);
+            this.Controls.Add(this.NoProductoNumericUpDown);
+            this.Controls.Add(this.label1);
+            this.Name = "rProductos";
+            this.Text = "rProductos";
+            ((System.ComponentModel.ISupportInitialize)(this.NoProductoNumericUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CantidadNumericUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MinimoNumericUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.UnidadNumericUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PrecioNumericUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CostoNumericUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GananciaNumericUpDown)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown NoProductoNumericUpDown;
+        private System.Windows.Forms.TextBox DescripcionTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.NumericUpDown CantidadNumericUpDown;
+        private System.Windows.Forms.NumericUpDown MinimoNumericUpDown;
+        private System.Windows.Forms.NumericUpDown UnidadNumericUpDown;
+        private System.Windows.Forms.NumericUpDown PrecioNumericUpDown;
+        private System.Windows.Forms.NumericUpDown CostoNumericUpDown;
+        private System.Windows.Forms.NumericUpDown GananciaNumericUpDown;
+        private System.Windows.Forms.Button NuevoButton;
+        private System.Windows.Forms.Button GuardarButton;
+        private System.Windows.Forms.Button EliminarButton;
+        private System.Windows.Forms.Button BuscarButton;
+    }
+}
