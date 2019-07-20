d6e3953d03fff1de10a4eac4a972159aff3b46f6
Juan Francisco Rosa Cuevas <juan_rosa1@ucne.edu.do>
2019-07-19 22:53:17 -0400

    Creando la ventana de registro de Suplidores

---
+++ UI/Registros/rSuplidores.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace ProyectoFinalAlpha.UI.Registros
+{
+    partial class rSuplidores
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BuscarButton = new System.Windows.Forms.Button();
+            this.EliminarButton = new System.Windows.Forms.Button();
+            this.GuardarButton = new System.Windows.Forms.Button();
+            this.NuevoButton = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.DescripcionTextBox = new System.Windows.Forms.TextBox();
+            this.NoProductoNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.RNCTextBox = new System.Windows.Forms.TextBox();
+            this.TelefonoTextBox = new System.Windows.Forms.TextBox();
+            this.DireccionTextBox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.NoProductoNumericUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BuscarButton
+            // 
+            this.BuscarButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.BuscarButton.Location = new System.Drawing.Point(195, 17);
+            this.BuscarButton.Name = "BuscarButton";
+            this.BuscarButton.Size = new System.Drawing.Size(75, 23);
+            this.BuscarButton.TabIndex = 39;
+            this.BuscarButton.Text = "Buscar";
+            this.BuscarButton.UseVisualStyleBackColor = true;
+            // 
+            // EliminarButton
+            // 
+            this.EliminarButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.EliminarButton.Location = new System.Drawing.Point(230, 244);
+            this.EliminarButton.Name = "EliminarButton";
+            this.EliminarButton.Size = new System.Drawing.Size(75, 54);
+            this.EliminarButton.TabIndex = 38;
+            this.EliminarButton.Text = "Eliminar";
+            this.EliminarButton.UseVisualStyleBackColor = true;
+            // 
+            // GuardarButton
+            // 
+            this.GuardarButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.GuardarButton.Location = new System.Drawing.Point(124, 244);
+            this.GuardarButton.Name = "GuardarButton";
+            this.GuardarButton.Size = new System.Drawing.Size(75, 54);
+            this.GuardarButton.TabIndex = 37;
+            this.GuardarButton.Text = "Guardar";
+            this.GuardarButton.UseVisualStyleBackColor = true;
+            // 
+            // NuevoButton
+            // 
+            this.NuevoButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.NuevoButton.Location = new System.Drawing.Point(15, 244);
+            this.NuevoButton.Name = "NuevoButton";
+            this.NuevoButton.Size = new System.Drawing.Size(75, 54);
+            this.NuevoButton.TabIndex = 36;
+            this.NuevoButton.Text = "Nuevo";
+            this.NuevoButton.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.label5.Location = new System.Drawing.Point(12, 184);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(37, 17);
+            this.label5.TabIndex = 26;
+            this.label5.Text = "RNC";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.label4.Location = new System.Drawing.Point(12, 146);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 17);
+            this.label4.TabIndex = 25;
+            this.label4.Text = "Telefono";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.label3.Location = new System.Drawing.Point(12, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 17);
+            this.label3.TabIndex = 24;
+            this.label3.Text = "Direccion";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.label2.Location = new System.Drawing.Point(12, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 17);
+            this.label2.TabIndex = 23;
+            this.label2.Text = "Nombre";
+            // 
+            // DescripcionTextBox
+            // 
+            this.DescripcionTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.DescripcionTextBox.Location = new System.Drawing.Point(115, 62);
+            this.DescripcionTextBox.Name = "DescripcionTextBox";
+            this.DescripcionTextBox.Size = new System.Drawing.Size(190, 23);
+            this.DescripcionTextBox.TabIndex = 22;
+            // 
+            // NoProductoNumericUpDown
+            // 
+            this.NoProductoNumericUpDown.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.NoProductoNumericUpDown.Location = new System.Drawing.Point(115, 17);
+            this.NoProductoNumericUpDown.Name = "NoProductoNumericUpDown";
+            this.NoProductoNumericUpDown.Size = new System.Drawing.Size(61, 23);
+            this.NoProductoNumericUpDown.TabIndex = 21;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 17);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "CodigoSuplidor";
+            // 
+            // RNCTextBox
+            // 
+            this.RNCTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.RNCTextBox.Location = new System.Drawing.Point(115, 200);
+            this.RNCTextBox.Name = "RNCTextBox";
+            this.RNCTextBox.Size = new System.Drawing.Size(190, 23);
+            this.RNCTextBox.TabIndex = 40;
+            // 
+            // TelefonoTextBox
+            // 
+            this.TelefonoTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.TelefonoTextBox.Location = new System.Drawing.Point(115, 146);
+            this.TelefonoTextBox.Name = "TelefonoTextBox";
+            this.TelefonoTextBox.Size = new System.Drawing.Size(190, 23);
+            this.TelefonoTextBox.TabIndex = 41;
+            // 
+            // DireccionTextBox
+            // 
+            this.DireccionTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.DireccionTextBox.Location = new System.Drawing.Point(115, 101);
+            this.DireccionTextBox.Name = "DireccionTextBox";
+            this.DireccionTextBox.Size = new System.Drawing.Size(190, 23);
+            this.DireccionTextBox.TabIndex = 42;
+            // 
+            // rSuplidores
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(328, 319);
+            this.Controls.Add(this.DireccionTextBox);
+            this.Controls.Add(this.TelefonoTextBox);
+            this.Controls.Add(this.RNCTextBox);
+            this.Controls.Add(this.BuscarButton);
+            this.Controls.Add(this.EliminarButton);
+            this.Controls.Add(this.GuardarButton);
+            this.Controls.Add(this.NuevoButton);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.DescripcionTextBox);
+            this.Controls.Add(this.NoProductoNumericUpDown);
+            this.Controls.Add(this.label1);
+            this.Name = "rSuplidores";
+            this.Text = "rSuplidores";
+            ((System.ComponentModel.ISupportInitialize)(this.NoProductoNumericUpDown)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BuscarButton;
+        private System.Windows.Forms.Button EliminarButton;
+        private System.Windows.Forms.Button GuardarButton;
+        private System.Windows.Forms.Button NuevoButton;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox DescripcionTextBox;
+        private System.Windows.Forms.NumericUpDown NoProductoNumericUpDown;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox RNCTextBox;
+        private System.Windows.Forms.TextBox TelefonoTextBox;
+        private System.Windows.Forms.TextBox DireccionTextBox;
+    }
+}
